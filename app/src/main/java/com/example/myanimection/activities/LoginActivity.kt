17fca71518b7afe51e3b2aa03c0a495941740229
Mario Ortiz <mario.ortiz.jibaja09@gmail.com>
2023-04-21 18:46:29 +0200

    Sistema de login y registro completamente funcional con integración de Google.

--- app/src/main/java/com/example/myanimection/activities/LoginActivity.kt
+++ app/src/main/java/com/example/myanimection/activities/LoginActivity.kt
@@ -1,36 +1,71 @@
 package com.example.myanimection.activities
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
-import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.text.method.TransformationMethod
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import com.afollestad.materialdialogs.MaterialDialog
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myanimection.R
 import com.example.myanimection.utils.Notifications
-import com.google.firebase.auth.ActionCodeSettings
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import org.w3c.dom.Text
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 enum class ProviderType{
     BASIC
 }
 
 class LoginActivity : AppCompatActivity() {
+
+    val GOOGLE_SIGNIN = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        var resultGoogleLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val data = result.data
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    val account = task.result
+                    if (account != null) {
+                        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                        FirebaseAuth.getInstance().signInWithCredential(credential)
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    var intent = Intent(this, HomeActivity::class.java)
+                                    startActivity(intent)
+                                } else {
+                                    Notifications.shortToast(
+                                        this,
+                                        "No se pudo iniciar sesión con Google."
+                                    )
+                                }
+                            }.addOnFailureListener {
+                                Notifications.shortToast(
+                                    this,
+                                    "No se pudo iniciar sesión con Google."
+                                )
+                            }
+                    }
+                }
+            }
+
+
         var isPassVisible = false
 
         val btnLogIn : Button = findViewById(R.id.btnLogin)
+        val btnGoogleLogIn: Button = findViewById(R.id.btnGoogleLogIn)
         val txtSignUp : TextView = findViewById(R.id.txtSignUp)
         val txtEmailSign : EditText = findViewById(R.id.txtEmailSignin)
         val txtPasswordSign : EditText = findViewById(R.id.txtPasswordSignin)
@@ -46,6 +81,10 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
         btnLogIn.setOnClickListener { signIn(txtEmailSign.text.toString(), txtPasswordSign.text.toString()) }
+
+        btnGoogleLogIn.setOnClickListener {
+            googleSignIn(resultGoogleLauncher)
+        }
         
         txtForgotPassword.setOnClickListener {
             val intent = Intent(this, PasswordRecoverActivity::class.java)
@@ -86,6 +125,17 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun googleSignIn(launcher: ActivityResultLauncher<Intent>) {
+        val conf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().build()
+
+        val client = GoogleSignIn.getClient(this, conf)
+        client.signOut()
+
+        launcher.launch(client.signInIntent)
     }
 
     private fun validateFields(email:String, password:String) : Boolean{
---
+++ app/src/main/java/com/example/myanimection/activities/PasswordRecoverActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myanimection.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.myanimection.R
+import com.example.myanimection.utils.Notifications
+import com.google.firebase.auth.FirebaseAuth
+
+class PasswordRecoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_recover)
+        supportActionBar?.hide()
+
+        val txtEmail: TextView = findViewById(R.id.txtEmailRecover)
+        val btnSendEmail: Button = findViewById(R.id.btnSendRecoverEmail)
+
+
+        btnSendEmail.setOnClickListener {
+            val email = txtEmail.text.toString()
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+            Notifications.alertDialogOK(this, "Recuperación de contraseña", "Se ha enviado un enlace al email para recuperar la cuenta." ,
+            positiveButtonClickListener = {onBackPressedDispatcher.onBackPressed()})
+        }
+
+    }
+}
